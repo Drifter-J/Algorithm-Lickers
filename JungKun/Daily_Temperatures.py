5a2a894fd69f3a4415a661b7d6cc3cfb8fef4086
toadwave <toad0475@gmail.com>
2019-09-22 19:10:31 +0900

    Create Daily_Temperatures.py

---
+++ JungKun/Daily_Temperatures.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        output = [0]*len(T)
+        stack=[]
+        for i in range(len(T)-1):
+            stack.append(i)
+            while stack and T[stack[-1]]<T[i+1]:
+                output[stack[-1]] = (i+1)-stack[-1]
+                stack.pop()               
+        return output
+                    
