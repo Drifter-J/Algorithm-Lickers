4afcc6e7aaeb27a0e15ff4e81e3199e7693cfd7d
toadwave <toad0475@gmail.com>
2019-10-16 10:03:55 +0900

    Update Custom_Sort_String.py

--- JungKun/Custom_Sort_String.py
+++ JungKun/Custom_Sort_String.py
@@ -1,15 +1,9 @@
-def solution(S:str,T:str)->str:
-    queue = list(S)
-    bucket = str()
-    for i,c in enumerate(T):
-        while i>0:
-
-        if  c == S[i]:
-            bucket += c
-            queue.pop(i)
-            i+=1
-    
-
-S="cba"
-T="abcd"
-solution(S,T)
+class Solution:
+    def customSortString(self, S: str, T: str) -> str:
+        l =[]
+        for i in S:
+            l.append(i*T.count(i))
+        for i in T:
+            if i not in S:
+                l.append(i)
+        return ''.join(l)
